11f586494df58ea85de90887c6f65b41ee24f423
TomDo <tom.do@bjmdigital.com.au>
2022-04-15 13:26:22 +1000

    added a check for non numbers in card number

--- app/Http/Controllers/ANZAPI.php
+++ app/Http/Controllers/ANZAPI.php
@@ -16,8 +16,15 @@ class ANZAPI extends Controller {
         $amount = $json['amount'];
         $date = date("Y-m-d");
         $time = date("H:i:s");
-        print json_encode(array("from" => array(array("card_number" => $cardnumber))
+
+        if (preg_match("/[^0-9 ]/", (string) $cardnumber)) { // (string) casting because $cardnumber actually xml object
+            return response()->json(['message' => 'Payment Failed'], 406);
+        }// if statement to check if it has non numbers in it, spaces are fine, sends 406 http code if fails
+
+
+
+        return response(json_encode(array("from" => array(array("card_number" => $cardnumber))
         ,"amount" => $amount,"transaction_number" => "abc12345",
-        "transaction_time" => "$date $time"));
+        "transaction_time" => "$date $time")));
     }
 }
--- app/Http/Controllers/NABAPI.php
+++ app/Http/Controllers/NABAPI.php
@@ -16,13 +16,18 @@ class NABAPI extends Controller {
         $amount = $xml->amount;
         $date = date("Y-m-d");
         $time = date("H:i:s");
-        print "<?xml version='1.0' encoding='UTF-8'?>
+
+        if (preg_match("/[^0-9 ]/", (string) $cardnumber)) { // (string) casting because $cardnumber actually xml object
+            return response()->json(['message' => 'Payment Failed'], 406);
+        }// if statement to check if it has non numbers in it, spaces are fine
+            
+        return response("<?xml version='1.0' encoding='UTF-8'?>
         <from>
           <card_number>$cardnumber</card_number>
         </from>
         <amount>$amount</amount>
         <transaction_number>abc1234</transaction_number>
         <transaction_time>$date $time</transaction_time>
-        ";
+        ");
     }
 }
