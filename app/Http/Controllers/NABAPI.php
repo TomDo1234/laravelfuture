42e02bc9469b790ec4e8957c97f98988e9eaa4b7
Tom Do <tomdo12345@gmail.com>
2022-04-15 10:31:43 +1000

    Removed env from git ignore to make for easier distribution

--- app/Http/Controllers/NABAPI.php
+++ app/Http/Controllers/NABAPI.php
@@ -2,11 +2,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
+use Controller;
 use Response;
 use Illuminate\Http\Request;
-
-use App\User;
 
 class NABAPI extends Controller {
 
